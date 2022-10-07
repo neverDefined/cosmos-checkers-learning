00c31188fa9c48402b756df243dcac9eb9b1b162
never <never@nevers-MacBook-Pro.local>
2022-10-07 13:26:44 -0400

    fix/compiler-issues

--- x/checkers/types/genesis.go
+++ x/checkers/types/genesis.go
@@ -9,8 +9,11 @@ const DefaultIndex uint64 = 1
 
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
+
 	return &GenesisState{
-		SystemInfo:     nil,
+		SystemInfo: SystemInfo{
+			NextId: uint64(DefaultIndex),
+		},
 		StoredGameList: []StoredGame{},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
